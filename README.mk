8219342b31b404f7804308d8ff2ada00eb002584
Andrea D'Amore <anddam@brapi.net>
2012-01-25 08:28:05 +0100

    adding a markdown written README

---
+++ README.mk
@@ -0,0 +1,5 @@
+* Twitter hosted app for Chrome
+
+The simplest app I could think of, I needed a big birdie bookmark in my Chrome app page.
+
+The Twitter icon is from [Twitter's resource page](https://twitter.com/about/resources/logos)
